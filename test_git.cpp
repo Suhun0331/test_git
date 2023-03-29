41f492771ef31a69c349172f650630687751f770
Suhun0331 <rlatngns6046@naver.com>
2023-03-29 15:26:51 +0900

    2_third

--- test_git.cpp
+++ test_git.cpp
@@ -1,12 +1,12 @@
-// 첫 번째 가상환경
+// 두 번째 가상환경
 
-// 두 번째 커밋
+// 세 번째 커밋
 
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "첫 번째 환경에서 두 번째로 등록한 코드입니다" << endl;
+    cout << "두 번째 환경에서 세 번째로 등록한 코드입니다" << endl;
     return 0;
 }
