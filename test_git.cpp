56d92d3923638cb9b2e381bc6647101fc476548b
Suhun0331 <104614675+Suhun0331@users.noreply.github.com>
2023-03-29 15:58:10 +0900

    1_conf2

--- test_git.cpp
+++ test_git.cpp
@@ -1,4 +1,4 @@
-// 두 번째 가상환경
+// 첫 번째 가상환경
 
 // 충돌 테스트
 
@@ -7,6 +7,7 @@
 using namespace std;
 
 int main() {
-    cout << "두 번째 가상환경 - 충돌이 일어날 구간입니다." << endl;
+    cout << "첫 번째 가상환경 - 충돌이 일어날 구간입니다." << endl;
+    cout << "첫 번째 가상환경 - 또 다른 github의 기능 확인" << endl;
     return 0;
 }
