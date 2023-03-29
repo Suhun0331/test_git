4fc9e0346ce62f716d95e337b4af40a79f5d3f76
nyj00 <84065527+nyj00@users.noreply.github.com>
2023-03-29 16:12:32 +0900

    Update test_git.cpp

--- test_git.cpp
+++ test_git.cpp
@@ -8,8 +8,5 @@ using namespace std;
 
 int main() {
     cout << "두 번째 가상환경 - 충돌이 일어날 구간입니다." << endl;
-    cout << "두 번째 가상환경 - 또 다른 github의 기능 확인" << endl;
-    cout << "첫 번째 가상환경 - 충돌이 일어날 구간입니다." << endl;
-    cout << "첫 번째 가상환경 - 또 다른 github의 기능 확인" << endl;
     return 0;
 }
