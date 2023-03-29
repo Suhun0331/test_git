a5ac568887785343690cc37a4e501bac8ca33271
Suhun0331 <rlatngns6046@naver.com>
2023-03-29 15:21:16 +0900

    1_second

--- test_git.cpp
+++ test_git.cpp
@@ -1,13 +1,12 @@
 // 첫 번째 가상환경
 
-// 첫 번째 커밋
+// 두 번째 커밋
 
 #include <iostream>
 
 using namespace std;
 
-
 int main() {
-    cout << "첫 번째로 등록한 코드입니다" << endl;
+    cout << "첫 번째 환경에서 두 번째로 등록한 코드입니다" << endl;
     return 0;
 }
